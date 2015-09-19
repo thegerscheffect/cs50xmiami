bc8c5637da8cd3c068a4c233b60b79c5924fc7a5
Your Name <you@example.com>
2015-09-18 20:48:05 -0400

    under construction

--- psets/9a.js
+++ psets/9a.js
@@ -1,12 +1,14 @@
-var value = 0;
-document.writeln("What is your sentence?");
+
 function pset9(words) {
+    if (!arguments[0]) {
+        document.writeln("What is your sentence?");
+    }
     var sentence = document.getElementById("userInput").value;
-    var length = sentence.length;
     var bad = sentence.indexOf("bad");
     var not = sentence.indexOf("not");
-    if(bad > not){
-        sentence = sentence.slice(0,not).concat("good!");
+    if (bad > not) {
+        sentence = sentence.slice(0, not).concat("good!");
     }
     document.getElementById("result").innerHTML = sentence;
-}pset9(value);
+}
+pset9();
