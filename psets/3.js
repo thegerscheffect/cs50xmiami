c349d91d991a3a826a24bd6bf7c2434230d648e0
Your Name <you@example.com>
2015-09-18 20:08:28 -0400

    fixed function calling error

--- psets/3.js
+++ psets/3.js
@@ -8,16 +8,16 @@ http://math2.org/math/geometry/circles.htm
 - Calculate the circumference based on the radius, and output "The circumference is NN".
 - Calculate the area based on the radius, and output "The area is NN".
 */
-
-// write your solution here...
-var circ, area;
-function geometrizer(area, circ){
-var radius = prompt("What is the radius of the circle?");
-var diameter = radius * radius;
-circ = 2 * Math.PI * radius;
-circ = Math.floor(circ * 100) / 100;
-document.write("The circumference is " + circ + ".");
-area = Math.PI * diameter;
-area = Math.floor(area * 100) / 100;
-document.write("\nThe area is " + area);
-}geometrizer(area, circ);
+function geometrizer(radius){
+    if(!arguments[0]){
+        radius = prompt("What is the radius of the circle?");
+    }
+    var diameter = radius * radius;
+    var circumference = 2 * Math.PI * radius;
+    circumference = Math.floor(circumference * 100) / 100;
+    document.write("The circumference of the circle is " + circumference + ".");
+    var area = Math.PI * diameter;
+    area = Math.floor(area * 100) / 100;
+    document.write("\nThe area of the circle is " + area);
+}
+geometrizer();
