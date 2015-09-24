58312b7ab956365f67313c8d62cf6751577c8fab
Your Name <you@example.com>
2015-09-24 13:47:51 -0400

    completed

--- psets/challenge-1.js
+++ psets/challenge-1.js
@@ -27,7 +27,7 @@ like Wheel of Fortune without the wheel and fortune).
   - Start with a reward amount of $0
   - Every time a letter is guessed, generate a random amount and reward the user if they found a letter (multiplying the reward if multiple letters found), otherwise subtract from their reward.
   - When they guess the word, log their final reward amount.
-
+2
 - Bonus: Make it like Hangman:
   - Keep track of all the guessed letters (right and wrong) and only let the user guess a letter once. If they guess a letter twice, do nothing.
   - Keep track of the state of the hangman as a number (starting at 0), and subtract or add to that number every time they make a wrong guess.
@@ -35,3 +35,31 @@ like Wheel of Fortune without the wheel and fortune).
 */
 
 // write your solution here...
+var word = "ABRA";
+var guess = [];
+
+for (var i = 0; i < word.length; i++) {
+  guess.push("_");
+}
+
+function guessLetter(letter) {
+  for (var i = 0; i < word.length; i++) {
+    if (letter.toUpperCase() === word[i]) {
+      guess[i] = word[i];
+      console.log(guess);
+      document.write("<h1>Congratulations you found a new letter.</h1>");
+    }
+  }
+  for (var j = 0; j < guess.length; j++) {
+    if (guess[j] === "_") {
+      return false;
+    }
+  }
+  return true;
+}
+
+do {
+  var done = guessLetter(prompt("You have not guessed the word\nGuess another letter."));
+} while (!done);
+
+document.getElementsByTagName("body")[0].innerHTML = "<h1>You guessed the word!</h1>";
